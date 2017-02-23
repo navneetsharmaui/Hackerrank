b86f0a5dd6a285852c96fba45deb8a684d80f292
navneetzz <navneetnnavneet1@gmail.com>
2017-02-23 19:22:32 +0530

    Added Week Of Code 29 Day Of Programmer and Big Sorting

---
+++ src/Contests/WeekOfCode29/BigSorting.java
@@ -0,0 +1,10 @@
+package Contests.WeekOfCode29;
+
+/**
+ * Created by ryu on 23/2/17.
+ */
+public class BigSorting {
+    public static void main(String[] args) {
+        
+    }
+}
--- src/Contests/WeekOfCode29/DayOfProgrammer.java
+++ src/Contests/WeekOfCode29/DayOfProgrammer.java
@@ -9,8 +9,33 @@ import java.util.regex.*;
  * Created by ryu on 23/2/17.
  */
 public class DayOfProgrammer {
+    private static void julianCalendar(int year) {
+        int feb=28, day;
+        if(year%4==0) {
+            feb=29;
+        }
+        day=256-(215+feb);
+        System.out.println(day+"."+"09"+"."+year);
+    }
+    private static void gregorianCalendar(int year) {
+        int feb=28, day;
+        if (year%4==0 || year%400==0) {
+            feb=29;
+        }
+        day = 256-(215+feb);
+        System.out.println(day+"."+"09"+"."+year);
+    }
+    private static void subtleYear(int year) {
+        System.out.println("26.09."+year);
+    }
+    private static void checkYear(int year) {
+        if (year < 1918) julianCalendar(year);
+        else if (year > 1918) gregorianCalendar(year);
+        else subtleYear(year);
+    }
     public static void main(String[] args) {
-        Scanner in = new Scanner();
+        Scanner in = new Scanner(System.in);
         int y = in.nextInt();
+        checkYear(y);
     }
 }
