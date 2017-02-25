b08fae29e07cc7a7097ec749f8425deddae92295
navneetzz <navneetnnavneet1@gmail.com>
2017-02-25 08:31:33 +0530

    Added output formatting code to java introduction

---
+++ src/Java/Introduction/JavaOutputFormatting.java
@@ -0,0 +1,20 @@
+package Java.Introduction;
+
+import java.util.Scanner;
+
+/**
+ * Created by ryu on 25/2/17.
+ */
+public class JavaOutputFormatting {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("================================");
+        for (int i=0;i<3;i++) {
+            String s = in.next();
+            int x = in.nextInt();
+            System.out.printf("%-15s%03d\n", s, x);
+        }
+        System.out.println("================================");
+    }
+}
