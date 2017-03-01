98c9584ea92f45ac30d1fc833c6b005f61c13386
navneetzz <navneetnnavneet1@gmail.com>
2017-03-01 14:48:27 +0530

    Added Java Comparator

---
+++ src/Java/DataStructures/JavaComparator.java
@@ -0,0 +1,37 @@
+package Java.DataStructures;
+
+import java.util.*;
+/**
+ * Created by ryu on 1/3/17.
+ */
+class Checker implements Comparator<Player> {
+    @Override
+    public int compare(Player p1, Player p2) {
+        if (p1.score==p2.score) return p1.name.compareTo(p2.name);
+        else return p2.score-p1.score;
+    }
+}
+class Player {
+    String name;
+    int score;
+
+    Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+public class JavaComparator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+
+        for (int i = 0; i < n; i++) player[i] = new Player(scan.next(), scan.nextInt());
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for (int i = 0; i < player.length; i++) System.out.println(player[i].name+" "+player[i].score);
+    }
+}
