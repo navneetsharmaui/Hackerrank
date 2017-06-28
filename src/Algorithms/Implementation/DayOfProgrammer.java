84fe1da64505e8d803e73cd1e3644542d77152ff
Navneet sharma <navneetnnavneet1@gmail.com>
2017-06-28 13:00:31 +0530

    Added updated Day of programmer code

--- src/Algorithms/Implementation/DayOfProgrammer.java
+++ src/Algorithms/Implementation/DayOfProgrammer.java
@@ -30,6 +30,7 @@ public class DayOfProgrammer {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int year = in.nextInt();
+        in.close();
         String result = solve(year);
         System.out.println(result);
     }
