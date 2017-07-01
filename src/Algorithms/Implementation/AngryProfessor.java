752be1e516ceb634fc7eef3a0b690ad8e7dac830
navneetzz <navneetnnavneet1@gmail.com>
2017-07-01 11:45:37 +0530

    Added code Angry Professor in the implementation of the algorithm track

--- src/Algorithms/Implementation/AngryProfessor.java
+++ src/Algorithms/Implementation/AngryProfessor.java
@@ -1,7 +1,28 @@
 package Algorithms.Implementation;
 
+import java.util.Scanner;
+
 /**
  * Created by Navneet Sharma (navneetzz) ryu on 1/7/17.
  */
 public class AngryProfessor {
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int i=0;i<t;i++) {
+            int n = in.nextInt();
+            int k = in.nextInt();
+            int count = 0;
+            for (int j=0;j<n;j++) {
+                int m = in.nextInt();
+                if (m<=0)
+                    count++;
+            }
+            if (count>=k)
+                System.out.println("NO");
+            else
+                System.out.println("YES");
+        }
+        in.close();
+    }
 }
