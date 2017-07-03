725251c329e60921a48966375e16c6b924991678
navneetzz <navneetnnavneet1@gmail.com>
2017-07-03 14:39:57 +0530

    Added code String Construction in the string section of the algorithm track

--- src/Algorithms/Strings/StringConstruction.java
+++ src/Algorithms/Strings/StringConstruction.java
@@ -5,30 +5,27 @@ import java.util.*;
 /**
  * Created by Navneet Sharma (navneetzz) ryu on 3/7/17.
  */
-public class TwoString {
-    static String twoStrings(String s1, String s2){
+public class StringConstruction {
+    static int stringConstruction(String s){
         int count = 0;
         HashMap<Character, Integer> map = new HashMap<>();
-        for (char ch : s1.toCharArray()){
+        for (char ch : s.toCharArray()) {
             if (map.containsKey(ch))
                 map.put(ch, map.get(ch)+1);
-            else
+            else {
                 map.put(ch, 1);
-        }
-        for (char ch : s2.toCharArray())
-            if (map.containsKey(ch))
                 count++;
-
-        return count>0?"YES":"NO";
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int q = in.nextInt();
         for(int a0 = 0; a0 < q; a0++){
-            String s1 = in.next();
-            String s2 = in.next();
-            String result = twoStrings(s1, s2);
+            String s = in.next();
+            int result = stringConstruction(s);
             System.out.println(result);
         }
         in.close();
